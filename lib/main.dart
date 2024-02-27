8e53775ef7b727f76403973dfab7ee029bd14e65
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-02-27 10:57:36 +0545

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'screens/categories.dart';
 
 final theme = ThemeData(
-useMaterial3: true,
-colorScheme: ColorScheme.fromSeed(brightness: Brightness.dark, seedColor: const Color.fromARGB(255, 131, 47, 0),),
-textTheme: GoogleFonts.latoTextTheme(),
-
-
-
-
-
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 47, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
 );
-
-
-
-
 
 void main() {
   runApp(const App());
@@ -28,11 +21,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
-theme: theme,
-//home: 
-
-
+      
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const Categories(),
     );
   }
 }
