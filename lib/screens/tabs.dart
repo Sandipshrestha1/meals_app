483be0e403972673d551e4aa1a49560a4f49f07a
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-03-01 02:05:16 +0545

    working to add filters

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -47,6 +47,20 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+if (identifier == 'filters'){
+
+} else {
+
+
+
+  Navigator.of(context).pop();
+}
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = Categories(
@@ -66,7 +80,7 @@ class _TabsScreenState extends State<TabsScreen> {
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer:const  MainDrawer(),
+      drawer: MainDrawer(onSelectScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
